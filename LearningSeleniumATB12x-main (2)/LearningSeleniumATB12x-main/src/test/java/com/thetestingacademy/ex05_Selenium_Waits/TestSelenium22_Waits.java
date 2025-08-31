9315d71a38458cad6c5ccb2f0a418521dbfaf8c4
monika-sharma08 <52006672+monika-sharma08@users.noreply.github.com>
2025-08-31 09:22:50 +0530

    Add files via upload

---
+++ LearningSeleniumATB12x-main (2)/LearningSeleniumATB12x-main/src/test/java/com/thetestingacademy/ex05_Selenium_Waits/TestSelenium22_Waits.java
@@ -0,0 +1,25 @@
+package com.thetestingacademy.ex05_Selenium_Waits;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+public class TestSelenium22_Waits {
+
+
+    @Description("Verify Ebay Print the prices (iMac)")
+    @Test
+    public void test_verify_print_imac_price() {
+
+        EdgeDriver driver = new EdgeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        driver.get("https://app.vwo.com");
+
+
+
+
+
+    }
+}
---
+++ LearningSeleniumATB12x-main (2)/LearningSeleniumATB12x-main/src/test/java/com/thetestingacademy/ex08_SVG_Elements/Lab30_SVG.java
@@ -0,0 +1,62 @@
+package com.thetestingacademy.ex08_SVG_Elements;
+
+import com.thetestingacademy.CommonToAll;
+import com.thetestingacademy.ex07_WaitHelper.WaitHelpers;
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Lab30_SVG extends CommonToAll {
+
+    ChromeDriver driver;
+
+    @Description("Verify that the on search with svg icon results are visible.")
+    @Test
+    public void test_Flipkart_Search_SVG_Icon() {
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        String URL = "https://www.flipkart.com/search";
+        driver.get(URL);
+
+        WebElement search_input = driver.findElement(By.name("q"));
+        search_input.sendKeys("macmini");
+
+        List<WebElement> svgElements = driver.findElements(By.xpath("//*[name()='svg']"));
+        svgElements.get(0).click();
+
+
+       WaitHelpers waitHelpers = new WaitHelpers();
+       waitHelpers.checkVisibility(driver,By.xpath("//div[contains(@data-id,'CPU')]/div/a[2]"));
+
+
+
+
+        List<WebElement> titlesResults = driver.findElements(By.xpath("//div[contains(@data-id,'CPU') or contains(@data-id,'MP')]/div/a[2]"));
+        for (WebElement title :  titlesResults){
+            System.out.println(title.getText());
+        }
+
+        WebElement next_button_page2 = driver.findElement(By.xpath("//a[normalize-space()='2']"));
+        next_button_page2.click();
+
+
+        List<WebElement> titlesResults2 = driver.findElements(By.xpath("//div[contains(@data-id,'CPU') or contains(@data-id,'MP')]/div/a[2]"));
+        for (WebElement title :  titlesResults2){
+            if(title.getText()!= null){
+                System.out.println(title.getText());
+            }
+
+        }
+
+
+        closeBrowser(driver);
+    }
+
+
+}
